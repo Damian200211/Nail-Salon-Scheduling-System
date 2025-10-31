8d9b30d052fe3ffdb298b6a730c4eb9376477d1c
Damian Le <Ledamian355@gmail.com>
2025-10-31 18:41:32 -0500

    chore(api): Register models with the Django admin

--- salon_backend/api/admin.py
+++ salon_backend/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Service, Technician, Customer, Appointment
+
+admin.site.register(Service)
+admin.site.register(Technician)
+admin.site.register(Customer)
+admin.site.register(Appointment)
